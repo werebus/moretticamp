a17375f75b5dbe405ca450c793cfd6fce7aba3a5
Matt Moretti <werebus@gmail.com>
2014-05-27 14:15:11 -0400

    Force devise actions over to SSL in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def url_for(options = nil)
+    if Hash === options
+      options[:protocol] ||= 'http'
+    end
+    super(options)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,25 @@ Rails.application.routes.draw do
   resources :events
   resources :seasons
 
-  devise_for :users, skip: [:registrations],
-    controllers: {omniauth_callbacks: 'users/omniauth_callbacks',
-                  invitations: 'users/invitations'}
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  if Rails.env.production?
+    scope :protocol => 'https://', :constraints => { :protocol => 'https://' } do
+      devise_for :users, skip: [:registrations],
+        controllers: {omniauth_callbacks: 'users/omniauth_callbacks',
+                      invitations: 'users/invitations'}
+      as :user do
+        get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+        put 'users' => 'devise/registrations#update', :as => 'user_registration'
+      end
+    end
+  else
+    devise_for :users, skip: [:registrations],
+      controllers: {omniauth_callbacks: 'users/omniauth_callbacks',
+                    invitations: 'users/invitations'}
+
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    end
   end
 
   authenticated :user do
