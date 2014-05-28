933f7155558db5436bfc290b4cf04ef41743f0fc
Matt Moretti <werebus@gmail.com>
2014-05-28 18:04:33 -0400

    Seasons can't overlap

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,22 @@
+class SeasonValidator < ActiveModel::Validator
+  def validate(record)
+    return if record.start_date.blank? or record.end_date.blank?
+
+    overlaps = Season.where("start_date IN (:range) OR end_date IN (:range)",
+                            range: record.date_range).reject{|s| s==record}
+
+    if overlaps.any?
+      record.errors[:base] << "Season overlaps with another season"
+    end
+  end
+end
+
 class Season < ActiveRecord::Base
   require 'date_range'
   include DateRange
+
+  validates_presence_of :start_date, :end_date
+  validates_with SeasonValidator
 
   class << self
     def current_or_next
