caa325b4f571ba5039d9580832a8f1a4b7162271
Matt Moretti <werebus@gmail.com>
2016-02-26 10:55:44 -0500

    Switch to SES from Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'moretti.camp' }
 
   config.action_mailer.smtp_settings = {
-    address:   'smtp.mandrillapp.com',
+    address:   'email-smtp.us-east-1.amazonaws.com',
     port:      587,
-    user_name: ENV['MANDRILL_USERNAME'],
-    password:  ENV['MANDRILL_API_KEY']
+    user_name: ENV['SES_USERNAME'],
+    password:  ENV['SES_PASSWORD']
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
