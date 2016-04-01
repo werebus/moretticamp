a19df6d6ddbdc67080e567dbfb048df72b582579
Matt Moretti <werebus@gmail.com>
2016-04-01 13:33:44 -0400

    Slightly better from field

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :ses
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = { from: 'noreply@moretti.camp' }
+  config.action_mailer.default_options = { from: 'Moretti.camp <noreply@moretti.camp>' }
   config.action_mailer.default_url_options = { host: 'moretti.camp' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
