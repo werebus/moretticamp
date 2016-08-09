78f8d15648f773c75602e8d76763ce61edd8690c
Matt Moretti <werebus@gmail.com>
2016-08-08 23:15:39 -0400

    Use fa icon() helper for oath icons

--- lib/oauth_providers.rb
+++ lib/oauth_providers.rb
@@ -2,19 +2,19 @@ OauthProvider = Struct.new :label, :name, :icon, :app_id, :app_secret
 
 google = OauthProvider.new :google_oauth2,
                            'Google',
-                           'fa-google',
+                           'google',
                            ENV['GOOGLE_APP_ID'],
                            ENV['GOOGLE_APP_SECRET']
 
 facebook = OauthProvider.new :facebook,
                              'Facebook',
-                             'fa-facebook',
+                             'facebook',
                              ENV['FACEBOOK_APP_ID'],
                              ENV['FACEBOOK_APP_SECRET']
 
 twitter = OauthProvider.new :twitter,
                             'Twitter',
-                            'fa-twitter',
+                            'twitter',
                             ENV['TWITTER_APP_ID'],
                             ENV['TWITTER_APP_SECRET']
 
