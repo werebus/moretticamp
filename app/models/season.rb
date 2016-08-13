64c8078f02d21948ed7a1078ed647ed92727fe19
Matt Moretti <werebus@gmail.com>
2016-08-12 20:53:21 -0400

    Fix duplicate month in printout
    When the season starts on the first of a month

--- app/models/season.rb
+++ app/models/season.rb
@@ -17,5 +17,6 @@ class Season < ActiveRecord::Base
   def months
     firsts = date_range.select{ |date| date.day == 1 }
     firsts.unshift start_date.beginning_of_month
+    firsts.uniq
   end
 end
