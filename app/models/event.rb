38806c1f0b34140ad0a72abd2639685e478c6d1e
Matt Moretti <werebus@gmail.com>
2018-06-07 23:28:06 -0400

    Guard clause, I guess?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,14 @@
 class EventSeasonValidator < ActiveModel::Validator
   def validate(record)
     return if record.start_date.blank? || record.end_date.blank?
+
     season = Season.current_or_next
-    if season.nil?
+    unless season.present?
       record.errors[:base] << 'Your event cannot be created because there is no season defined yet.'
       return
     end
-    unless season.date_range.include?(record.date_range)
-      record.errors[:base] << "Event must occur durring the current season (#{season.date_range_words})."
-    end
+    return if season.date_range.include?(record.date_range)
+    record.errors[:base] << "Event must occur durring the current season (#{season.date_range_words})."
   end
 end
 
