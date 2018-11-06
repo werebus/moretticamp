db93a4821c1e95c98a9608b916acda6aa46c47a4
Matt Moretti <werebus@gmail.com>
2018-11-06 15:25:45 -0500

    Calendar spec

---
+++ spec/system/calendar_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'EventsController#index - calendar', js: true do
+  let! :season do
+    create :season, :now
+  end
+  let! :event do
+    create :event, :now, title: 'LADYBUGS'
+  end
+  before :each do
+    sign_in(create :user)
+    visit events_path
+  end
+
+  it 'starts on the current month' do
+    header = find('#calendar .fc-header-toolbar .fc-left h2')
+    expect(header).to have_content Date.today.strftime('%B %Y')
+  end
+  it 'has an event on it' do
+    within '#calendar .fc-body' do
+      expect(page).to have_content event.display_title
+      expect(page).to have_link href: /#{event_path(event)}/
+    end
+  end
+  it 'limits navigation to the season' do
+    Capybara.enable_aria_label = true
+    within '#calendar' do
+      until page.has_css? 'button.fc-prev-button[disabled]' do
+        click_on 'prev'
+      end
+      expect(page).to have_content season.start_date.strftime('%B %Y')
+      until page.has_css? 'button.fc-next-button[disabled]' do
+        click_on 'next'
+      end
+      expect(page).to have_content season.end_date.strftime('%B %Y')
+    end
+  end
+  it 'has camp open and close events on it' do
+    Capybara.enable_aria_label = true
+    within '#calendar' do
+      until page.has_css? 'button.fc-prev-button[disabled]' do
+        click_on 'prev'
+      end
+      expect(page).to have_content 'Camp Opens'
+      until page.has_css? 'button.fc-next-button[disabled]' do
+        click_on 'next'
+      end
+      expect(page).to have_content 'Camp Closes'
+    end
+  end
+end
