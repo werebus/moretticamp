ed488c329061558fe46160973d97125715d0b301
Matt Moretti <werebus@gmail.com>
2018-11-16 16:38:14 -0500

    Small config tweaks to RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,12 +6,11 @@ SimpleCov.start 'rails'
 require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
-require 'capybara/rails'
-require 'capybara/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 ActiveRecord::Migration.maintain_test_schema!
-Capybara.server = :webrick
+Capybara.server = :puma
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
@@ -20,11 +19,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-
   config.before(:each, type: :system, js: true) do
     driven_by :selenium, using: :firefox
   end
-
-  config.include Devise::Test::IntegrationHelpers, type: :request
-  config.include Devise::Test::IntegrationHelpers, type: :system
 end
