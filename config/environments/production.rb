fc7f9103e6e7fea16213992d2e94d13992c22372
Matt Moretti <werebus@gmail.com>
2021-01-08 13:36:10 -0500

    Rubocop fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require "active_support/core_ext/integer/time"
+
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in
@@ -44,7 +45,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
