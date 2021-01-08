66143629be6e6035a5ccdac38f73e4ce044c61ba
Matt Moretti <werebus@gmail.com>
2021-01-08 15:54:05 -0500

    Add a 'perfoming_the_job' context

--- spec/jobs/notification_sender_job_spec.rb
+++ spec/jobs/notification_sender_job_spec.rb
@@ -21,46 +21,46 @@ RSpec.describe NotificationSenderJob do
         .to have_enqueued_job.on_queue('default')
     end
 
-    it 'converts the body to HTML', perform_enqueued: true do
-      expect(NotificationMailer).to receive(:notification_email)
-        .with(anything, anything, %r{<(strong|b)>Testing</\1>})
-        .at_least(:once)
-        .and_return(dummy_mail_message)
-      described_class.perform_later(note_params)
-    end
+    context 'performing the job', perform_enqueued: true do
+      it 'converts the body to HTML' do
+        expect(NotificationMailer).to receive(:notification_email)
+          .with(anything, anything, %r{<(strong|b)>Testing</\1>})
+          .at_least(:once)
+          .and_return(dummy_mail_message)
+        described_class.perform_later(note_params)
+      end
 
-    it 'passes User.to_notify the override parameter', perform_enqueued: true do
-      expect(User).to receive(:to_notify)
-        .with(override: true).once.and_return([])
-      expect(User).to receive(:to_notify)
-        .with(override: false).once.and_return([])
+      it 'passes User.to_notify the override parameter' do
+        expect(User).to receive(:to_notify)
+          .with(override: true).once.and_return([])
+        expect(User).to receive(:to_notify)
+          .with(override: false).once.and_return([])
 
-      described_class.perform_later(note_params.merge(override: true))
-      described_class.perform_later(note_params.merge(override: false))
-    end
+        described_class.perform_later(note_params.merge(override: true))
+        described_class.perform_later(note_params.merge(override: false))
+      end
 
-    it <<~DESC, perform_enqueued: true do
-      sends a notification to each user who wants notifications
-    DESC
-      expect(NotificationMailer).to receive(:notification_email)
-        .twice.and_return(dummy_mail_message)
+      it 'sends a notification to each user who wants notifications' do
+        expect(NotificationMailer).to receive(:notification_email)
+          .twice.and_return(dummy_mail_message)
 
-      described_class.perform_later(note_params)
-    end
+        described_class.perform_later(note_params)
+      end
 
-    it 'sends a notification to all users if told to', perform_enqueued: true do
-      expect(NotificationMailer).to receive(:notification_email)
-        .exactly(5).times.and_return(dummy_mail_message)
+      it 'sends a notification to all users if told to' do
+        expect(NotificationMailer).to receive(:notification_email)
+          .exactly(5).times.and_return(dummy_mail_message)
 
-      described_class.perform_later(note_params.merge(override: true))
-    end
+        described_class.perform_later(note_params.merge(override: true))
+      end
 
-    it 'uses the subject given', perform_enqueued: true do
-      expect(NotificationMailer).to receive(:notification_email)
-        .with(anything, 'TEST EMAIL', anything)
-        .at_least(:once)
-        .and_return(dummy_mail_message)
-      described_class.perform_later(note_params)
+      it 'uses the subject given' do
+        expect(NotificationMailer).to receive(:notification_email)
+          .with(anything, 'TEST EMAIL', anything)
+          .at_least(:once)
+          .and_return(dummy_mail_message)
+        described_class.perform_later(note_params)
+      end
     end
   end
 end
