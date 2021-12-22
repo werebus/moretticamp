3d046882c0bc2ce94c4c1557283a46ad7f8ee98c
Matt Moretti <werebus@gmail.com>
2021-12-22 10:14:14 -0500

    Everything...works.
    Calendar needs to be restyled, and I need to get my old foundation
    settings back.

---
+++ app/javascript/events.js
@@ -0,0 +1,55 @@
+import { Calendar, formatDate } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+$(document).ready(function() {
+  const caldate = function() {
+    const date = Number(window.location.hash.slice(1)) ||
+      $('#calendar').data('date') * 1000;
+    return new Date(date);
+  };
+
+  var calEl = document.getElementById('calendar')
+  if(calEl) {
+    var calendar = new Calendar(calEl, {
+      plugins: [ dayGridPlugin ],
+      eventSources: [
+        {
+          url: '/events.json',
+          className: 'event-event',
+          error(e){
+            if (e.status === 401) { window.location.reload(false); }
+          }
+        },{
+          url: '/seasons.json',
+          className: 'season-event'
+        }
+      ],
+      initialDate: caldate(),
+      validRange: {
+        start: new Date( $('#calendar').data('start-date') * 1000),
+        end: new Date( $('#calendar').data('end-date') * 1000)
+      },
+      buttonIcons: false,
+      buttonText: { today: 'Today' },
+      titleFormat: function(date) {
+        var opts;
+        if (Foundation.MediaQuery.is('medium')) {
+          opts = { month: 'long', year: 'numeric' };
+        } else {
+          opts = { month: 'numeric', year: '2-digit' };
+        }
+        return formatDate(date.date.marker, opts);
+      },
+
+      eventDidMount(info){
+        const desc = info.event.extendedProps.description
+
+        if (desc) {
+          var tip = new Foundation.Tooltip($(info.el), { tipText: desc });
+        }
+      }
+    });
+
+    calendar.render();
+  }
+});
--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 Time::DATE_FORMATS[:ics] = ->(time) { time.utc.strftime '%Y%m%dT%H%M%SZ' }
+Date::DATE_FORMATS[:unix] = ->(date) { date.to_time(:local).to_i }
