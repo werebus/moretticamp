82b5e432b9f87e558646c0405d3e3f3cee770b51
Matt Moretti <werebus@gmail.com>
2022-01-02 16:57:26 -0500

    App update task

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,39 +1,33 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
-  policy.base_uri    :self
-  policy.default_src :self
-  policy.font_src    :self, :data
-  policy.img_src     :self, :data
-  policy.object_src  :none
-  policy.script_src  :self
-  policy.style_src   :self
-  policy.connect_src :self
+Rails.application.configure do
+  config.content_security_policy do |policy|
+   policy.default_src :self
+   policy.font_src    :self, :data
+   policy.img_src     :self, :data
+   policy.object_src  :none
+   policy.script_src  :self
+   policy.style_src   :self
+   policy.connect_src :self
 
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
+   # Specify URI for violation reports
+   # policy.report_uri "/csp-violation-report-endpoint"
 
-  # If you are using webpack-dev-server then specify webpack-dev-server host
-  break unless Rails.env.development?
+   break unless Rails.env.development?
 
-  policy.connect_src :self, 'http://localhost:3035', 'ws://localhost:3035'
+   policy.connect_src :self, 'http://localhost:3035', 'ws://localhost:3035'
+ end
+
+ # Generate session nonces for permitted importmap and inline scripts
+ config.content_security_policy_nonce_generator =
+   ->(request) { request.session.id.to_s }
+ config.content_security_policy_nonce_directives = %w[script-src]
+
+ # Report CSP violations to a specified URI. See:
+ # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+ # config.content_security_policy_report_only = true
 end
-
-# If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator =
-  ->(_request) { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-Rails.application.config.content_security_policy_nonce_directives =
-  %w[script-src]
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
