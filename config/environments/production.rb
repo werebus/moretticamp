a6f5ce35a79cd13e8c072b881377aa4ed5494b9e
Matt Moretti <werebus@gmail.com>
2024-06-11 16:13:25 -0400

    The gem only reads a few keys from credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Aws.config.update(region: 'us-east-1')
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
