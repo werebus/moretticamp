ead5fb93cd27babfae45b4ebfecd5bbc8cf98f2a
Matt Moretti <werebus@gmail.com>
2024-11-15 17:33:29 -0500

    Fix tooltips on main page
    And clipboard button

--- app/javascript/global.js
+++ app/javascript/global.js
@@ -1,17 +1,22 @@
+import { Tooltip } from 'bootstrap';
 import $ from 'jquery';
 
 const ClipboardJS = require('clipboard');
 
 $(document).ready(function() {
-  const clipboard_selector = '.button.clip';
-  const clipboard_button = $(clipboard_selector);
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+
+  const clipboard_selector = 'button.clip';
+  const clipboard_button = document.querySelector(clipboard_selector);
   const clipboard = new ClipboardJS(clipboard_selector);
+
   clipboard.on('success', function() {
     $(clipboard_selector).attr('title', 'Copied!');
-    const tip = new Foundation.Tooltip(clipboard_button);
+    const tip = new Tooltip(clipboard_button);
     tip.show();
     setTimeout(function() {
-      tip._destroy();
+      tip.dispose();
       $(clipboard_selector).attr('title', null);
     }
     , 2000);
