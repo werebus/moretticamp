aff13e4e98323e964685969f3d48732a388d7262
Matt Moretti <werebus@gmail.com>
2024-11-18 22:44:21 -0500

    Use Rails's built-in PWA controller

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,5 @@ Rails.application.routes.draw do
     get "/#{action}", to: "pages##{action}", as: "#{action}_page"
   end
 
-  get '/manifest.json', to: 'pwa#manifest', as: 'pwa_manifest'
+  get '/manifest.json', to: 'rails/pwa#manifest', as: 'pwa_manifest'
 end
