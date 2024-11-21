98606c342114326c61b75c865efd4f362f961a54
Matt Moretti <werebus@gmail.com>
2024-11-21 11:04:19 -0500

    Proabably should have run the specs: syntax error

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,5 +3,5 @@
 aws_credential_keys = %i[access_key_id secret_access_key session_token account_id]
 creds = Rails.application.credentials[:aws].to_h.slice(*aws_credential_keys)
 
-options = creds.merge{ region: 'us-east-1' }
+options = creds.merge({ region: 'us-east-1' })
 ActionMailer::Base.add_delivery_method :ses, Aws::ActionMailer::SES::Mailer, **options
