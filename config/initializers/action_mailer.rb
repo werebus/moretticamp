fba52b1b1ae3705d7beeb8f30a58a66afe25c869
Matt Moretti <werebus@gmail.com>
2024-12-06 09:02:59 -0500

    Configure SES using `config.action_mailer.ses_settings`
    With 1.0.0, we don't have to register our own delivery method anymore
    (again).

--- config/initializers/action_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-aws_credential_keys = %i[access_key_id secret_access_key session_token account_id]
-creds = Rails.application.credentials[:aws].to_h.slice(*aws_credential_keys)
-
-options = creds.merge({ region: 'us-east-1' })
-ActionMailer::Base.add_delivery_method :ses, Aws::ActionMailer::SES::Mailer, **options
