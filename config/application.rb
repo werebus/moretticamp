cfd2e760855d93d5186a5a947ec68c7257d8c2d7
Matt Moretti <werebus@gmail.com>
2025-03-21 11:28:24 -0400

    Load Rails 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Moretticamp
   class Application < Rails::Application
     require 'oauth_provider'
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.before_initialize do
       OauthProvider.new :google_oauth2, 'Google', 'google',
@@ -36,13 +36,5 @@ module Moretticamp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     # config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
