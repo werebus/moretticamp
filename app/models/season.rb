9efe2cf90b0d7b46313e399e7708c8ae4831aebf
Matt Moretti <werebus@gmail.com>
2025-03-30 18:12:58 -0400

    Consolidate season validator (#296)
    No need for a whole other class for just this one validation

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,11 +2,20 @@
 
 class Season < ApplicationRecord
   include DateRange
-  validates_with SeasonValidator
+  validate :no_overlaps
 
   def months
     firsts = date_range.select { |date| date.day == 1 }
     firsts.unshift start_date.beginning_of_month
     firsts.uniq
+  end
+
+  private
+
+  def no_overlaps
+    overlaps = Season.find_by('start_date < ? AND end_date > ? AND id != ?', end_date, start_date, id.to_i)
+    return if overlaps.blank?
+
+    errors.add(:base, 'Season overlaps with another season')
   end
 end
