9051398ad6722b830ef116cf7bdf7a9943fbed59
Matt Moretti <werebus@gmail.com>
2025-03-30 13:15:09 -0400

    Add "active" users
    Part of #229, adds the concept of "active user" of the site: they've
    accepted their invitation and made at least one event in the last 5
    years.
    
    Also fixed a bug in the "full name" if users only have first names

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,22 @@ class User < ApplicationRecord
   validates :first_name, presence: true, if: -> { last_name.blank? }
   validates :calendar_access_token, uniqueness: { case_sensitive: true }
 
-  def self.find_for_oauth(auth)
-    find_by(provider: auth.provider, uid: auth.uid)
-  end
+  class << self
+    def active
+      invitation_accepted.joins(:events).where(events: { start_date: (5.years.ago..) }).distinct
+    end
 
-  def self.to_notify(override: false)
-    override ? all : where(email_updates: true)
+    def find_for_oauth(auth)
+      find_by(provider: auth.provider, uid: auth.uid)
+    end
+
+    def to_notify(override: false)
+      override ? all : where(email_updates: true)
+    end
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    [first_name, last_name].join(' ').strip
   end
 
   def invitation_limit
