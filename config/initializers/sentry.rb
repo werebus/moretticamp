6b718db9b21026dbd69823e1aa243a3f34dd1e34
Matt Moretti <werebus@gmail.com>
2025-04-21 11:41:07 -0400

    Initial Sentry setup

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.breadcrumbs_logger = [:active_support_logger]
+  config.dsn = "https://c19a3cc721b0690f4e3cd46684281381@o4509191950303232.ingest.us.sentry.io/4509191952400384"
+  config.traces_sample_rate = 1.0
+end
