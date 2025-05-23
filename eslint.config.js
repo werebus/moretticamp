2032f9093dcf620562451ed89cf66a22f620c8dd
Matt Moretti <werebus@gmail.com>
2025-05-23 14:54:40 -0400

    Exclude vendored Gems
    (Action cable has some huge JS files)

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,10 @@ import neostandard from 'neostandard'
 
 export default neostandard({
   env: ['browser'],
-  ignores: ['node_modules', 'app/assets', 'coverage'],
+  ignores: [
+    'app/assets',
+    'coverage',
+    'node_modules',
+    'vendor',
+  ],
 })
