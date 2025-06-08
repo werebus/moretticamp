4d992a1a19a0776586a53592a48e7e224481f3de
Matt Moretti <werebus@gmail.com>
2025-06-08 12:35:26 -0400

    Load Rails 8.0 defaults (#391)

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Moretticamp
   class Application < Rails::Application
     require 'oauth_provider'
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     config.before_initialize do
       OauthProvider.new :google_oauth2, 'Google', 'google',
