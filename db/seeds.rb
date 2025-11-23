d74839687590f448d9da04db847828849cb260b1
Matt Moretti <werebus@gmail.com>
2025-11-23 14:15:06 -0500

    Add some seed data
    Closes #562

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database
-# with its default values.  # The data can then be loaded with the rake db:seed
-# (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+exit unless Rails.env.development?
+
+User.create! email: 'admin@example.com', password: 'Password1', password_confirmation: 'Password1', admin: true,
+             first_name: Faker::Name.first_name, last_name: Faker::Name.last_name
+
+users = Array.new(10) do
+  password = Faker::Internet.password(min_length: 8)
+  User.create! email: Faker::Internet.unique.email,
+               password:, password_confirmation: password,
+               first_name: Faker::Name.first_name, last_name: Faker::Name.last_name
+end
+
+Season.create! available: 2.years.ago, start_date: 1.year.ago, end_date: 1.year.from_now
+
+15.times do
+  start_date = Faker::Date.between(from: 6.months.ago, to: 6.months.from_now)
+  end_date = start_date + [0, 0, 0, 0, 0, 1, 2, 3, 4, 7].sample.days
+  Event.create! start_date:, end_date:, user: users.sample
+end
+
+3.times do
+  date = Faker::Date.between(from: 6.months.ago, to: 6.months.from_now)
+  Event.create! start_date: date, end_date: date, title: Faker::Lorem.sentence(word_count: 3), user: nil
+end
